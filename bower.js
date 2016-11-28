8b904a4a790184faf0e283f0113b7ce868f50e6f
Adrien Dufond <adrien.dufond@gmail.com>
2016-11-28 12:17:01 +0900

    Create bower.js

---
+++ bower.js
@@ -0,0 +1,19 @@
+{
+  "name": "manmaru.loremipsumutil",
+  "main": "manmaru.loremipsumutil.js",
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/adrien-dufond/manmaru.loremipsumutil.git"
+  },
+  "authors": [
+    "Adrien Dufond <adrien.dufond@gmail.com>"
+  ],
+  "description": "javascript utilities for generate random text.",
+  "keywords": [
+    "lorem",
+    "ipsum",
+    "manmaru"
+  ],
+  "license": "MIT",
+  "homepage": "www.manmaru.fr"
+}
